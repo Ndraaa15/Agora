118ada4f9744a7b0ccf73c8cbde35b8a79c546b1
Theresia Artha Putri <therenoona@gmail.com>
2024-05-24 00:52:16 +0700

    responsive signup

--- resources/views/web/auth/signup.blade.php
+++ resources/views/web/auth/signup.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.auth')
 @section('content')
-<div class="flex flex-row w-full h-screen">
-    <div class="w-1/2 h-full">
-        <img src="{{ asset('assets/images/signup.jpg') }}" alt="Signin Image" class="w-full h-full object-cover">
+<div class="flex flex-col lg:flex-row w-full h-screen">
+    <div class="lg:w-1/2 h-full">
+        <img src="{{ asset('assets/images/signup.jpg') }}" alt="Signin Image" class="w-full h-screen lg:h-full object-cover object-bottom">
     </div>
-    <div class="w-1/2 flex items-center justify-center p-8">
+    <div class="lg:w-1/2 flex items-center justify-center p-8 lg:mt-20">
         <div >
-            <h1 class="text-5xl font-bold mb-10">Sign Up</h1>
+            <h1 class="text-5xl font-bold mb-10 mt-20">Sign Up</h1>
             <form action="{{ route('signup-post' )}}" method="POST">
                 @csrf
                 @method('POST')
@@ -17,7 +17,7 @@
                     <input type="text" placeholder="Phone Number" name="phone" class="w-96 px-4 py-2 border  rounded-md mb-4 shadow-md outline-none">
                     <input type="password" placeholder="Password" name="password" class="w-96 px-4 py-2 border  rounded-md mb-4 shadow-md outline-none">
                     <input type="password" placeholder="Re-enter password" name="re_password" class="w-96 px-4 py-2 border  rounded-md mb-4 shadow-md outline-none">
-                    <div class="flex flex-row justify-between mb-5 mt-4">
+                    <div class="flex flex-row justify-between mb-5 mt-2">
                         <button class="px-8 py-2 flex flex-row gap-5 rounded-md align-middle shadow-md hover:bg-gray-50">
                             <img src="{{ asset('assets/icons/google.png') }}" alt="Google" class="w-5 h-5">
                             <p class="text-sm">Google</p>
@@ -30,7 +30,7 @@
                 </div>
                 <button type="submit" class="w-full bg-blue-400 text-white font-medium px-4 py-2 rounded-md hover:bg-blue-500">Create Account</button>
             </form>
-            <p class="text-sm text-gray-400 text-center mt-5">Already have an account? <a href="{{ route('signin') }}" class="hover:text-gray-500">Sign in</a></p>
+            <p class="text-sm text-gray-400 text-center mt-5 mb-5">Already have an account? <a href="{{ route('signin') }}" class="hover:text-gray-500">Sign in</a></p>
         </div>
     </div>
 </div>
