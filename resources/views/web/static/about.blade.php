05489d80051c07f7c284cfbd39ef39afe0cd42b0
audynr <audynadiraramadanti@gmail.com>
2024-05-25 21:37:04 +0700

    fix page about us

--- resources/views/web/static/about.blade.php
+++ resources/views/web/static/about.blade.php
@@ -7,16 +7,16 @@
     <img src="{{ asset('assets/images/cloud2.png') }}" alt="cloud2" class="flex absolute z-1 right-0 sm:w-[120px] md:w-[150px] lg:w-[180px] xl:w-[210px] 2xl:w-[250px]">
 
     <div class="bg-white w-full">
-        <div class="flex justify-center 2xl:mt-[60px] sm:mt-[30px]">
+        <div class="flex justify-center 2xl:mt-[60px] sm:mt-[30px] xl:mt-[50px]">
             <h1 class="text-[#279EFF] font-semibold text-center 2xl:text-[44px] xl:text-[38px] lg:text-[36px] md:text-[32px] sm:text-[26px]">About Us</h1>
         </div>
     </div>
 
     <div class="flex w-full 2xl:mt-[120px] sm:mt-[70px] sm:flex-col sm:justify-center sm:items-center">
-        <div class="row relative">
-            <div class="2xl:w-[720px] xl:w-[600px] lg:w-[500px] md:w-[400px] sm:w-[300px] absolute 2xl:left-[100px] xl:left-[100px] sm:left-[20px]">
+        
+            <div class="2xl:w-[720px] xl:w-[600px] lg:w-[500px] md:w-[400px] sm:w-[300px] absolute 2xl:left-[150px] 2xl:top-[350px] xl:left-[100px] sm:left-[20px] sm:top-[180px] md:left-[40px] md:top-[200px] lg:left-[120px] lg:top-[220px] xl:left-[200px] xl:top-[300px]">
                 <h1 class="text-[#0C356A] font-semibold 2xl:text-[36px] xl:text-[30px] lg:text-[28px] md:text-[24px] sm:text-[18px] text-left pt-[10px] pb-[10px]">About Agora</h1>
-                <p class="text-justify font-normal text-[14px] leading-[20px] 2xl:leading-[40px] xl:leading-[35px] lg:leading-[30px] md:leading-[25px] sm:leading-[20px] 2xl:w-[580px] sm:w-[300px] md:w-[320px] lg:w-[350px] xl:w-[480px] mt-[10px]">
+                <p class="text-justify font-normal text-[14px] leading-[20px] 2xl:leading-[40px] xl:leading-[35px] lg:leading-[30px] md:leading-[25px] sm:leading-[20px] 2xl:w-[580px] sm:w-[300px] md:w-[320px] lg:w-[350px] xl:w-[480px] mt-[10px] ">
                 Agora is a ticketing website designed to boost public participation in cultural activities. It simplifies the ticket purchasing process and offers comprehensive information about cultural events, making it easier for people to discover and attend these events. This streamlined approach enhances accessibility and engagement in cultural experiences.
                 </p>
                 <div class="flex flex-row mt-[20px] sm:text-[14px]">
@@ -26,12 +26,11 @@
                     <button class="w-[75px] h-[30px] sm:w-[50px] border-2 border-solid border[#6D758F] text-[#0d6efd] rounded-[20px] flex justify-center items-center mr-[10px] hover:bg-[#0d6efd] hover:text-white">Art</button>
                 </div>
             </div>
-            <div class="absolute right-0">
-                <img src="{{ asset('assets/images/Intersect1.svg') }}" alt="Image 1" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[450px] sm:right-[160px] md:right-[200px] lg:right-[290px] xl:right-[430px] xl:top-[70px] sm:top-[20px] z-1">
-                <img src="{{ asset('assets/images/intersect2.svg') }}" alt="Image 2" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[190px] sm:right-[20px] md:right-[30px] lg:right-[100px] xl:right-[200px] xl:top-[100px] sm:top-[40px] z-1">
-                <img src="{{ asset('assets/images/intersect3.svg') }}" alt="Image 3" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[310px] sm:right-[80px] md:right-[110px] lg:right-[190px] xl:right-[320px] 2xl:top-[200px] sm:top-[120px] md:top-[150px] lg:top-[170px] xl:top-[210px] z-2">
-            </div>
-        </div>
+            
+                <img src="{{ asset('assets/images/Intersect1.svg') }}"  alt="Image 1" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[450px] 2xl:top-[350px] sm:right-[160px] md:right-[200px] lg:right-[290px] xl:right-[430px] xl:top-[330px] sm:top-[200px] md:top-[260px] sm:top-[200px] z-1">
+                <img src="{{ asset('assets/images/intersect2.svg') }}" alt="Image 2" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[190px] 2xl:top-[400px] sm:right-[20px] md:right-[30px] lg:right-[100px] xl:right-[200px] xl:top-[380px] sm:top-[240px] md:top-[300px] z-1">
+                <img src="{{ asset('assets/images/intersect3.svg') }}" alt="Image 3" class="absolute 2xl:w-[230px] sm:w-[120px] md:w-[150px] lg:w-[170px] xl:w-[200px] 2xl:right-[310px] 2xl:top-[550px] sm:right-[80px] md:right-[110px] lg:right-[190px] xl:right-[320px] 2xl:top-[200px] xl:top-[470px] sm:top-[300px] md:top-[380px] lg:top-[400px] xl:top-[210px] z-2">
+            
     </div>
 
     <h1 class="text-[#0C356A] font-semibold text-center w-full pb-4 2xl:text-4xl xl:text-[30px] lg:text-[28px] md:text-[24px] sm:text-[18px] 2xl:mt-[500px] sm:mt-[270px] md:mt-[350px] lg:mt-[380px] xl:mt-[450px]">Our Vision</h1>
